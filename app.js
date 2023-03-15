2a5f22e915f7769494cadd957ece73267670c6f7
Nitroceliulioze <aksionova.diana@gmail.com>
2023-03-15 17:42:05 +0200

    Add students to local storage

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-// DOM Elements
+// DOM elements
 const studentForm = document.getElementById("studentForm");
 const studentsContainer = document.querySelector(".students");
 const nameInput = studentForm["name"];
 const ageInput = studentForm["age"];
 const rollInput = studentForm["roll"];
-
-/* 
-{
-  name: '',
-  age: number,
-  roll: number
-}
-*/
 
 const students = JSON.parse(localStorage.getItem("students")) || [];
 
@@ -28,29 +20,28 @@ const addStudent = (name, age, roll) => {
 };
 
 const createStudentElement = ({ name, age, roll }) => {
-  // Create elements
+  //create elements
   const studentDiv = document.createElement("div");
   const studentName = document.createElement("h2");
   const studentAge = document.createElement("p");
   const studentRoll = document.createElement("p");
 
-  // Fill the content
+  //fill elements
   studentName.innerText = "Student name: " + name;
   studentAge.innerText = "Student age: " + age;
   studentRoll.innerText = "Student roll: " + roll;
 
-  // Add to the DOM
+  //add to DOM
   studentDiv.append(studentName, studentAge, studentRoll);
   studentsContainer.appendChild(studentDiv);
-
-  studentsContainer.style.display = students.length === 0 ? "none" : "flex";
+  studentsContainer.style.display =
+  students.length === 0 ? "none" : "flex";
 };
-
-studentsContainer.style.display = students.length === 0 ? "none" : "flex";
-
+studentsContainer.style.display =
+  students.length === 0 ? "none" : "flex";
 students.forEach(createStudentElement);
 
-studentForm.onsubmit = e => {
+studentForm.onsubmit = (e) => {
   e.preventDefault();
 
   const newStudent = addStudent(
